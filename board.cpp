047115fc68b153f723833950daf09c0f4a1248d5
KevinIssa <kevin.issa@ulb.be>
2022-11-12 20:10:41 +0100

    win condition et check moves

--- board.cpp
+++ board.cpp
@@ -52,9 +52,12 @@ class Board{
     }
 
     void load_level();
+    bool check_moves(vector_2D actual_player_position, int push_direction);
     bool win_condition();
-    bool check_moves();
-    void play_move();
+    int id (int x, int y) { 
+        return y * get_level_size().x + x; 
+    }
+    void play_move(vector_2D actual_position, vector_2D final_position);
 
 };
 
@@ -71,12 +74,16 @@ void Board::load_level(){
                         level_vector.emplace_back(make_unique < Wall>(vector_2D{x,y},block_size.x,block_size.y));
                         break;
                     case '$':
-                        level_vector.emplace_back(make_unique < Light_block>(vector_2D{x,y},block_size.x,block_size.y));
+                        level_vector.emplace_back(make_unique < Light_box>(vector_2D{x,y},block_size.x,block_size.y));
                         break;
                     case '@':
                         level_vector.emplace_back(make_unique < Player>(vector_2D{x,y},block_size.x,block_size.y));
                         player_position.x = x; player_position.y = y;
                         break;
+                    case '+':
+                        level_vector.emplace_back(make_unique < Box>(vector_2D(x,y),block_size.x,block_size.y));
+                        break;
+
                     case '.':
                         level_vector.emplace_back(nullptr);
                         objectives_list.push_back({x, y});
@@ -90,34 +97,53 @@ void Board::load_level(){
     }
 };
 
-bool Board::win_condition(){
-            tuple current_block = player_position;
-            bool allow_pushing = false;
-            bool test = true;
-            
-            while(test)  //test in one way
+bool Board::check_moves(vector_2D actual_player_position, int push_direction){
+    vector_2D current_block = actual_player_position;
+    // function who attribuate a value (x,y) as an id for a cell
+    bool allow_pushing = false;
+    bool stop_flag = true;
+    
+    while(stop_flag)  //test in one way
+    {
+        //check if the block we want to go is empty
+        if (level_vector [id(current_block.x,current_block.y)] != nullptr)
+        {
+            //check if the block can be pushed
+            if (level_vector[id(current_block.x,current_block.y)]->push((push_direction + 2) % 4))
             {
-                if (level_vector [id(current_block)] != nullptr)
+                //block can move
+                switch (push_direction)
                 {
-                    if (level_vector[id(current_block)]->push((push_direction + 2) % 4))
-                    {
-                        //bloc peut bouger
-                        switch (push_direction)
-                        {
-                            case NORTH: current_block.y--; break;
-                            case SOUTH: current_block.y++; break;
-                            case EAST: current_block.x++; break;
-                            case WEST: current_block.x--; break;
-                        }
-                        // if (current_block == Heavy_block && current_block + 1 is_empty)
-                    }
-                    else
-                        test = false;
-                }
-                else
-                {
-                    allow_pushing = true;
-                    test = false;
+                    case NORTH: current_block.y--; break;
+                    case SOUTH: current_block.y++; break;
+                    case EAST: current_block.x++; break;
+                    case WEST: current_block.x--; break;
                 }
             }
+            else
+                stop_flag = false;
+        }
+        else
+        {
+            allow_pushing = true;
+            stop_flag = false;
+        }
+    }
+    return allow_pushing;
+}
+
+bool Board::win_condition(){
+    int goals_win=0;
+    for (auto& position_index: objectives_list)
+    {
+        if (level_vector[id(position_index.x,position_index.y)] 
+        && level_vector[id(position_index.x,position_index.y)].isbox())
+        goals_win++;
+    }
+
+    return goals_win == objectives_list.size();
+}
+
+void Board:: play_move(vector_2D actual_position, vector_2D final_position){
+    
 }
