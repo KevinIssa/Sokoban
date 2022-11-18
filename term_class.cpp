9ac5b22490079d9681279f380a6e49cf03b3571a
Mickael Kovel <90647742+mk0v3l@users.noreply.github.com>
2022-11-18 05:42:13 +0000

    first class

--- term_class.cpp
+++ term_class.cpp
@@ -14,15 +14,34 @@ const int WEST = 3;
 
 class Case
 {
-private:
-string name;
-const char repr;
+    private:
+        string name;
+        char repr;
+        char value;
 
 
-public:
-Case(string name, const char repr);
-string get_name(){return name;}
-const char get_repr(){return repr;}
+    public:
+    // Case(string name, const char repr);
+        Case(string name, char value): name{name},value{value},repr{value}{}
+        Case(string name, char value, char repr): name{name},value{value},repr{repr}{}
+
+        string get_name(){return name;}
+        const char get_repr(){return repr;}
+        // void draw(){printf("%d",get_repr());}
+        const char get_value(){return value;}
+        const char draw(){return repr;}
+        void set_repr(char r){repr=r;}
+        void set_value(char r){value=r;}
+
+
+
+
+};
+
+class Player : public Case
+{
+    public:
+        Player(string name, char value): Case(name,value){}
 
 };
 
@@ -45,12 +64,12 @@ void init(vector<string> &data_level, tup &size_level)
         "##########"
         "#        #"
         "#  $     #"
-        "#      . #"
+        "#    + . #"
         "#  * $ $ #"
-        "#    @   #"
+        "#  ++@+  #"
         "#   +    #"  
         "#  ++    #"
-        "# .      #"
+        "# .$     #"
         "##########";struct tup size_level_t= {10,10};
 
     string level =
@@ -99,6 +118,7 @@ void init(vector<string> &data_level, tup &size_level)
 void load_game(tup &pos_player, tup &size_level, string level_s, vector <tup> &goals_v, tup &original_pos, vector<Case> &level_c)
 {
     goals_v.clear();
+    level_c.clear();
     for (int y=0;y<size_level.y;y++)
     {
         for (int x =0; x< size_level.x; x++)
@@ -107,7 +127,8 @@ void load_game(tup &pos_player, tup &size_level, string level_s, vector <tup> &g
             {
                 case '@':
                 {
-                    Case player =Case{"player",level_s[y * size_level.x + x]};
+                    Player player{"Player",level_s[y * size_level.x + x]};
+                    
                     level_c.push_back(player);
                     pos_player.x = x; 
                     pos_player.y = y;
@@ -115,8 +136,10 @@ void load_game(tup &pos_player, tup &size_level, string level_s, vector <tup> &g
                     break;
                 }
                 case '.':
-                {
-                    Case obj{"objective",level_s[y * size_level.x + x]};
+                {   
+                    // Case obj{"Objective",level_s[y * size_level.x + x]};
+                    Case obj{"Objective",level_s[y * size_level.x + x]},' '};
+                    // Objective obj{"Objective",level_s[y * size_level.x + x]};
                     level_c.push_back(obj);
                     goals_v.push_back(tup{x,y});
                     level_s[id(x,y)]=' ';
@@ -124,20 +147,45 @@ void load_game(tup &pos_player, tup &size_level, string level_s, vector <tup> &g
                 }
                 case '$':
                 {
-                    Case box_h{"Boxe legere",level_s[y * size_level.x + x]};
+                    Case box_h{"Heavy Box",level_s[y * size_level.x + x]};
                     level_c.push_back(box_h);
+                    break;
+                }
+                case '#':
+                {
+                    Case wall{"Wall",level_s[y * size_level.x + x]};
+                    level_c.push_back(wall);
+                    break;
+                }
+                case '+':
+                {
+                    Case light_case{"BLight Box",level_s[y * size_level.x + x]};
+                    level_c.push_back(light_case);
+                    break;
                 }
                 default:
                 {
                     Case free_case{"free",' '};
                     level_c.push_back(free_case);
-            }   }
-        }
+                }   
+            }
+        }   
     }
 }
 
-bool listen_key(tup &pos_player, char ch, tup &current_pos, int &push_dir, int niveau, string &level_s, vector<string> data_level, tup original_pos)
+int print_game(vector<Case>&level_c, vector <tup>&goals_v, tup size_level)
 {
+    for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()==' '){printf("yoyy");level_c[id(g.x, g.y)].set_repr('.');}}
+    int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.draw();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;
+    for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()=='.'){level_c[id(g.x, g.y)].set_repr(' ');}}
+    int count=0;for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()=='$'){count++;}}printf("%d / %lu\n", count, goals_v.size());
+    return count;
+}   
+
+bool listen_key(tup &pos_player, char ch, tup &current_pos, int &push_dir, int niveau, string &level_s, vector<string> data_level, tup original_pos,vector<Case> &level_c, vector<Case> &original_level)
+{
+    printf("\npos_player =(%d,%d)\n",pos_player.x,pos_player.y);
+
     bool test = false;
     bool allow_pushing=false;
     switch (ch)
@@ -163,7 +211,13 @@ bool listen_key(tup &pos_player, char ch, tup &current_pos, int &push_dir, int n
         test = true;
         break;  
     case 'r':
-        level_s = data_level[niveau];
+        // level_s = data_level[niveau];
+        level_c.clear();
+        for (auto&c:original_level)
+        {
+            level_c.push_back(c);
+        }
+        // level_c = original_level;
         pos_player.x=original_pos.x;
         pos_player.y=original_pos.y;
         printf("RESET LVL ...");
@@ -175,53 +229,116 @@ bool listen_key(tup &pos_player, char ch, tup &current_pos, int &push_dir, int n
     return test;
 }
 
-bool check_move(bool& test, string &level_s, tup &current_pos, int push_dir)
+bool check_move(bool& test, string &level_s, tup &current_pos, int push_dir, vector<Case> &level_c)
 {   
+    int verif=0;
     bool allow_pushing=false;
     while(test)
     {
-        if (level_s[id(current_pos.x, current_pos.y)] != '#')
+        switch (level_c[id(current_pos.x, current_pos.y)].get_value())
         {
-            if (level_s[id(current_pos.x, current_pos.y)] != ' ' &&  (level_s[id(current_pos.x, current_pos.y)] != '$'))
-            {   
-                switch (push_dir)
-                {
-                    case NORTH: current_pos.y--; break;
-                    case SOUTH: current_pos.y++; break;
-                    case EAST: current_pos.x++; break;
-                    case WEST: current_pos.x--; break;
-                }
+        case '$':
+            verif++;
+            if(verif >1){test=false;break;} //stop condition to not stop pushing a heavy box
+            switch (push_dir)
+            {
+                case NORTH: 
+                    test = level_c[id(current_pos.x, current_pos.y--)].get_value() == ' ';
+                    break;
+                case SOUTH: 
+                    test = level_c[id(current_pos.x, current_pos.y++)].get_value() == ' ';
+                    break;
+                case EAST: 
+                    test = level_c[id(current_pos.x++, current_pos.y)].get_value() == ' ' ;
+                    break;
+                case WEST: 
+                    test = level_c[id(current_pos.x--, current_pos.y)].get_value() == ' ' ;
+                    break;
             }
-            else if (level_s[id(current_pos.x, current_pos.y)] == '$')
-            {  
-                switch(push_dir)
-                {
-                    case NORTH: current_pos.y--; break;
-                    case SOUTH: current_pos.y++; break;
-                    case EAST: current_pos.x++; break;
-                    case WEST: current_pos.x--; break;
-                }
-                if (level_s[id(current_pos.x, current_pos.y)] == ' ')
-                {
-                    test = false;
-                    allow_pushing = true;
-                }
-                else
-                    test = false; 
-            }
-            else
-            { 
-                allow_pushing = true;
-                test=false;
-            }
-        }
-        else
+
+            allow_pushing = level_c[id(current_pos.x, current_pos.y)].get_value() == ' ';
             test=false;
+            break;
+        case '#':
+            test=false;
+            break;
+        case '+':
+            verif++;
+            switch (push_dir)
+            {
+                case NORTH: current_pos.y--; break;
+                case SOUTH: current_pos.y++; break;
+                case EAST: current_pos.x++; break;
+                case WEST: current_pos.x--; break;
+            }
+            break;
+        case ' ':
+            allow_pushing = true;
+            test=false;
+            break;
+
+        }
     }
     return allow_pushing;
 }
 
-void play_move(tup &current_pos, tup &pos_player, int push_dir, string& level_s)
+
+
+            // if (level_c[id(current_pos.x, current_pos.y)].get_value() == ' ')
+            // {
+            //     test = false;
+            //     allow_pushing = true;
+            // }
+            // else
+            // {
+            //     test=false;
+            // }
+    //             printf("\ncurrent pos=(%d,%d)\n",current_pos.x,current_pos.y);
+    //     // if (level_s[id(current_pos.x, current_pos.y)] != '#')
+    //     if (level_c[id(current_pos.x, current_pos.y)].get_value() != '#')
+    //     {
+    //         if (level_c[id(current_pos.x, current_pos.y)].get_value() != ' ' &&  (level_c[id(current_pos.x, current_pos.y)].get_value() != '$'))
+    //         {   
+    //             switch (push_dir)
+    //             {
+    //                 case NORTH: current_pos.y--; break;
+    //                 case SOUTH: current_pos.y++; break;
+    //                 case EAST: current_pos.x++; break;
+    //                 case WEST: current_pos.x--; break;
+    //             }
+    //         }
+    //         else if (level_c[id(current_pos.x, current_pos.y)].get_value() == '$')
+    //         {  
+    //             switch(push_dir)
+    //             {
+    //                 case NORTH: current_pos.y--; break;
+    //                 case SOUTH: current_pos.y++; break;
+    //                 case EAST: current_pos.x++; break;
+    //                 case WEST: current_pos.x--; break;
+    //             }
+            
+    //             if (level_c[id(current_pos.x, current_pos.y)].get_value() == ' ')
+    //             {
+    //                 test = false;
+    //                 allow_pushing = true;
+    //             }
+    //             else
+    //              printf("oui");
+    //                 test = false; 
+    //         }
+    //         else
+    //         { 
+    //             allow_pushing = true;
+    //             test=false;
+
+    //         }
+    //     }
+    //     else
+    //         test=false;
+    //                 printf("non");
+
+
+void play_move(tup &current_pos, tup &pos_player, int push_dir, string& level_s, vector<Case> &level_c)
 {
     while (current_pos.x != pos_player.x || current_pos.y != pos_player.y)
     {
@@ -233,7 +350,11 @@ void play_move(tup &current_pos, tup &pos_player, int push_dir, string& level_s)
             case EAST: source.x--; break;
             case WEST: source.x++; break;
         }
-        swap(level_s[id(source.x, source.y)], level_s[id(current_pos.x, current_pos.y)]);
+        swap(level_c[id(source.x, source.y)], level_c[id(current_pos.x, current_pos.y)]);
+        // swap(level_c[0], level_c[26]);
+            // iter_swap(level_c.begin() + id(source.x, source.y), level_c.begin() + id(current_pos.x, current_pos.y));
+
+
         current_pos.x = source.x;
         current_pos.y = source.y;
     }
@@ -256,8 +377,14 @@ void play(vector<string> data_level, tup size_level)
     int niveau =0 ;
     string level_s = data_level[niveau];
     load_game(pos_player, size_level, level_s, goals_v, original_pos, level_c);
+    for (auto &g:goals_v){printf("\ngooooiX:%d,gogcgfgY:%d\n",g.x,g.y);}
+    vector<Case> original_level;
+    for (auto&c:level_c)
+        {
+            original_level.push_back(c);
+        }
 
-    // printf("\n");printf("SOKOBAN - GAME - level %d\n",niveau+1);
+    printf("\n");printf("SOKOBAN - GAME - level %d\n",niveau+1);
     // for (auto &g:goals_v){if (level_s[id(g.x, g.y)]==' '){level_s[id(g.x, g.y)]='.';}}
     // for (auto &c:level_s){print_lvl+=c;i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}
     // cout <<print_lvl;
@@ -270,17 +397,35 @@ void play(vector<string> data_level, tup size_level)
 
 
     // printf("\n");printf("SOKOBAN - GAME - level %d\n",niveau+1);for (auto &g:goals_v){if (level_s[id(g.x, g.y)]==' '){level_s[id(g.x, g.y)]='.';}}int i=0;string print_lvl="";for (auto &c:level_s){print_lvl+=c;i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;for (auto &g:goals_v){if (level_s[id(g.x, g.y)]=='.' ){level_s[id(g.x, g.y)]=' ';}}int count =0;for (auto &g:goals_v){if (level_s[id(g.x, g.y)]=='$'){count++;}}printf("%d / %lu\n", count, goals_v.size());
-    int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.get_repr();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}
-    cout <<print_lvl;
+    
+    // for (auto &g:goals_v){cout << g.x<<g.y<<endl;}
+    // for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()==' '){level_c[id(g.x, g.y)].set_repr('.');}}
+    // int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.draw();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;
+    // for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()=='.'){level_c[id(g.x, g.y)].set_repr(' ');}}
+    // int count=0;for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()=='$'){count++;}}printf("%d / %lu\n", count, goals_v.size());
+    print_game(level_c,goals_v,size_level);
+    
     cin >> ch;
     while(ch!='`')
     {   
         tup current_pos = pos_player;
         int push_dir;
-        bool test = listen_key(pos_player, ch, current_pos, push_dir, niveau, level_s, data_level, original_pos);
-        if (check_move(test, level_s, current_pos, push_dir))
-            play_move(current_pos, pos_player, push_dir, level_s);
-        printf("\n");printf("\n");printf("SOKOBAN - GAME - level %d\n",niveau+1);for (auto &g:goals_v){if (level_s[id(g.x, g.y)]==' '){level_s[id(g.x, g.y)]='.';}}int i=0; string print_lvl="";for (auto &c:level_s){print_lvl+=c;i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;for (auto &g:goals_v){if (level_s[id(g.x, g.y)]=='.' ){level_s[id(g.x, g.y)]=' ';}} int count =0;for (auto &g:goals_v){if (level_s[id(g.x, g.y)]=='$' || level_s[id(g.x, g.y)]=='+'){count++;}}printf("%d / %lu\n", count, goals_v.size());
+        bool test = listen_key(pos_player, ch, current_pos, push_dir, niveau, level_s, data_level, original_pos,level_c, original_level);
+        printf("\nbefore check: pos_player =(%d,%d)\n",pos_player.x,pos_player.y);
+
+        if (check_move(test, level_s, current_pos, push_dir, level_c))
+            play_move(current_pos, pos_player, push_dir, level_s, level_c);
+        // printf("\n");printf("\n");printf("SOKOBAN - GAME - level %d\n",niveau+1);for (auto &g:goals_v){if (level_s[id(g.x, g.y)]==' '){level_s[id(g.x, g.y)]='.';}}int i=0; string print_lvl="";for (auto &c:level_s){print_lvl+=c;i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;for (auto &g:goals_v){if (level_s[id(g.x, g.y)]=='.' ){level_s[id(g.x, g.y)]=' ';}} int count =0;for (auto &g:goals_v){if (level_s[id(g.x, g.y)]=='$' || level_s[id(g.x, g.y)]=='+'){count++;}}printf("%d / %lu\n", count, goals_v.size());
+        // int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.get_repr();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}
+        // int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.draw();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;
+        
+        // for (auto &g:goals_v){cout << g.x<<g.y<<endl;}
+        // for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()==' '){level_c[id(g.x, g.y)].set_repr('.');}}
+        // int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.draw();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;
+        // int count=0;for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()=='$'){count++;}}printf("%d / %lu\n", count, goals_v.size());
+        // for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()=='.'){level_c[id(g.x, g.y)].set_repr(' ');}}
+        int count = print_game(level_c, goals_v,size_level);
+
         if (count == goals_v.size())
         {   
             niveau++;
@@ -289,8 +434,20 @@ void play(vector<string> data_level, tup size_level)
             printf("--> go to lvl %d\n",niveau+2);
             level_s = data_level[niveau];
             load_game(pos_player, size_level, level_s, goals_v, original_pos, level_c);
+            original_level.clear();
+            for (auto &c:level_c){original_level.push_back(c);}
             // printf("\n");printf("\n");printf("SOKOBAN - GAME - level %d\n",niveau+1);for (auto &g:goals_v){if (level_s[id(g.x, g.y)]==' '){level_s[id(g.x, g.y)]='.';}}int i=0; string print_lvl="";for (auto &c:level_s){print_lvl+=c;i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;for (auto &g:goals_v){if (level_s[id(g.x, g.y)]=='.' ){level_s[id(g.x, g.y)]=' ';}} int count =0;for (auto &g:goals_v){if (level_s[id(g.x, g.y)]=='$' || level_s[id(g.x, g.y)]=='+'){count++;}}printf("%d / %lu\n", count, goals_v.size());
-            int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.get_repr();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}
+            // int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.draw();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}
+            
+
+            // for (auto &g:goals_v){cout << g.x<<g.y<<endl;}
+            // for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()==' '){level_c[id(g.x, g.y)].set_repr('.');}}
+            // int i=0;string print_lvl="";for (auto &c:level_c){print_lvl+=c.draw();i++;if (i==size_level.x ){print_lvl+="\n";i=0;}}cout <<print_lvl;
+            // int count=0;for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()=='$'){count++;}}printf("%d / %lu\n", count, goals_v.size());
+            // for (auto &g:goals_v){if (level_c[id(g.x, g.y)].get_repr()=='.'){level_c[id(g.x, g.y)].set_repr(' ');}}
+            print_game(level_c, goals_v,size_level);
+
+        
         }
 
 
@@ -316,7 +473,5 @@ int main()
     printf("YOU WIN !!\n"); 
     return 0;
 }
-
-
 
 
