e7c75f86751935c88637df591177d140c5a5de6b
mk0v3l <mickaelkovel@hotmail.com>
2022-11-20 03:03:48 +0100

    ..

--- fl.cpp
+++ fl.cpp
@@ -1,3 +1,4 @@
+// g++ -std=c++11 fl.cpp -o fl -lfltk && ./fl 
 #include <FL/Fl.H>
 #include <FL/fl_draw.H>
 #include <FL/Fl_Double_Window.H>
@@ -26,15 +27,15 @@ class MainWindow : public Fl_Window
 void draw() override 
         {
             Fl_Window::draw();
-            // vector<tup> ve;int c=0;int d;for (int i=0;i<10;i++){for (int j=0;j<10;j++){   d=(i+j)%2;ve.push_back(tup{i,j});}}for (auto &g:ve){   int d = (g.x+g.y)%2;if (d){fl_draw_box(FL_FLAT_BOX, g.x*50, g.y*50, 50, 50, FL_GREEN);}else {fl_draw_box(FL_FLAT_BOX, g.x*50, g.y*50, 50, 50, FL_BLACK);}}
+            vector<tup> ve;int c=0;int d;for (int i=0;i<10;i++){for (int j=0;j<10;j++){   d=(i+j)%2;ve.push_back(tup{i,j});}}for (auto &g:ve){   int d = (g.x+g.y)%2;if (d){fl_draw_box(FL_FLAT_BOX, g.x*50, g.y*50, 50, 50, FL_GREEN);}else {fl_draw_box(FL_FLAT_BOX, g.x*50, g.y*50, 50, 50, FL_BLACK);}}
             // Mat image = imread("Pok1.png", 1);
             // unsigned char *ptr_source = image.data;
-            unsigned char *buf;
+            // unsigned char *buf;
 
             // Fl_Draw_Image_Cb("pokeball.jpeg", 50, 50, -1 buf);
             // fl_draw_image(buf, 10,10, 500, 500);
 
-            fl_draw_image((const unsigned  char *)("poke"), 10,10, 500, 500);
+            // fl_draw_image((const unsigned  char *)("poke"), 10,10, 500, 500);
         }
         
 };
