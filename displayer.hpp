edfd6984ed1beca766039e51738734cde5bf6165
mk0v3l <mickaelkovel@hotmail.com>
2023-01-07 03:07:51 +0100

    Sat  7 Jan 03:07:51 CET 2023

--- displayer.hpp
+++ displayer.hpp
@@ -7,7 +7,22 @@ class Displayer
 {   
     private:
         Sokoban *soko;
-       
+        vector <Vector2D> normal_goals;
+        vector <Vector2D> purple_goals;
+        vector <Vector2D> yellow_goals;
+        vector <Vector2D> teleporter_cells;
+        vector <Vector2D> box_list;
+        const char * player_image="player.png";
+        /* vector < <vector*> Vector2D > cells_game = {&normal_goals, &purple_goals, &yellow_goals, &yellow_goals, &teleporter_cells, &box_list }; */
+
+        Vector2D player_pos; 
+        /* vector <const char> representation = {".","/","*","&","$","H","M","+"}; */
+        vector <char> representation = {'.','/','*','&','$','H','M','+'};
+        vector<const char *>image= {"pika.png", "elekable.png", "giratina.png", "teleporter.png","pokeball.png", "hyper_ball.png","master_ball.png", "superball.png" };
+        
+        /* const char reprr [] = [ '.' , '/', '*', '&', '$','H','M','+']; */
+        vector<Fl_Image> images_cells = {normal_goals_im, yellow_obj_im };
+        /* Fl_Image[] image_cell = [normal_goals_im, yellow_obj_im  ]; */   
     public:
         Displayer(Sokoban *soko) : soko{soko}{}
         ~Displayer()=default;
@@ -24,7 +39,7 @@ class Displayer
         void draw_menu();
 
 };
-
+/* [ ] */
 
 #endif
 
